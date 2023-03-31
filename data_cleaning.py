b89bd2ca25b0e73b5ca3e8bcf4b227403261feb9
Thibaut Charre <thibaut.charre.tc@gmail.com>
2023-03-31 17:17:57 +0200

    Continue working on data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,7 +6,6 @@
 import data_dl_func as dl_func
 import data_cleaning_func as clean_func
 import pandas as pd
-import seaborn as sns
 
 ###
 # DL & direct filter of datas (as we only want to study players' shots)
@@ -27,7 +26,7 @@ df_full_datas = pd.merge(df_play, df_players,
 # Check if a player is missing
 ###
 if len(df_full_datas[df_full_datas['Age'].isna()]) == 0:
-    print ('No missing player in players infos files')
+    print('No missing player in players infos files')
 
 ###
 # Study of df_full_datas architectures
@@ -51,7 +50,9 @@ df_full_datas2 = df_full_datas[[
 ###
 # Cleaning play_length variable: Transform time strings into doubles
 ###
-df_full_datas2['play_length'] = df_full_datas2['play_length'].apply(lambda row: int(row[-2:]))
+df_full_datas2['play_length'] = df_full_datas2['play_length'].apply(
+    lambda row: int(row[-2:])
+)
 df_full_datas2['play_length'].value_counts().sort_index().plot(kind='bar')
 
 # We notice play_length > 24 secs which is an anomaly, we eliminate them from df
@@ -60,19 +61,53 @@ df_full_datas2 = df_full_datas2[df_full_datas2['play_length'] <= 24]
 ###
 # Cleaning elapsed variable: Transform time strings into doubles
 ###
-df_full_datas2['elapsed_float'] = df_full_datas2.apply(lambda row: clean_func.elapsed_to_float(row['period'], row['elapsed']), axis=1)
+df_full_datas2['elapsed_float'] = df_full_datas2.apply(
+    lambda row: clean_func.elapsed_to_float(row['period'], row['elapsed']),
+    axis=1
+)
 
 ###
-# Cleaning: Define area regarding shots coordinates
+# Cleaning & Feature: Re-arrange x and y coordinates & define area regarding shots coordinates
 ###
+df_full_datas2['x_shot'] = df_full_datas2.apply(
+    lambda row: clean_func.x_shots(row['converted_x'], row['converted_y'], row['shot_distance']),
+    axis=1
+)
+
+df_full_datas2['y_shot'] = df_full_datas2.apply(
+    lambda row: clean_func.y_shots(row['converted_y'], row['shot_distance']),
+    axis=1
+)
+
+df_full_datas2['area_shot'] = df_full_datas2.apply(
+    lambda row: clean_func.area_defined(row['shot_distance'], row['x_shot'], row['y_shot'], row['type']),
+    axis=1
+)
 
 ###
-# Feature: Check if a player plays at home or away
+# Feature: Check if a player plays at home or away & calculate the game scoring difference
+# before a player make the bucket or not
 ###
+df_full_datas2['player_home'] = df_full_datas2.apply(
+    lambda row: 1 if row['player'] in '%s-%s-%s-%s-%s' % (row['h1'], row['h2'], row['h3'], row['h4'], row['h5']) else 0,
+    axis=1
+)
+
+df_full_datas2['player_away'] = df_full_datas2.apply(
+    lambda row: 1
+    if row['player'] not in '%s-%s-%s-%s-%s' % (row['h1'], row['h2'], row['h3'], row['h4'], row['h5'])
+    else 0,
+    axis=1
+)
+
+df_full_datas2['player_game_score'] = (df_full_datas2['player_home'] - df_full_datas2['player_away']) * \
+                                      (df_full_datas2['home_score'] - df_full_datas2['away_score']) - \
+                                      df_full_datas2['points']
 
 ###
 # Feature: Calculate nb of shots consecutively made/missed by player before taking a shot
 ###
+
 
 ###
 # Feature: Calculate nb of shots consecutively made/missed by player before taking a shot
@@ -89,8 +124,3 @@ df_full_datas2['elapsed_float'] = df_full_datas2.apply(lambda row: clean_func.el
 ###
 # Feature: Calculate number of assists/non assists received by a player before taking a shot
 ###
-
-
-
-
-
--- data_cleaning_func.py
+++ data_cleaning_func.py
@@ -7,3 +7,56 @@ def elapsed_to_float(period, time_string):
     period_time = min(period-1, 4) * 12 + (max(period, 5) - 5) * 5
 
     return time_float + period_time
+
+
+# Functions to gather all possible shots into a semi-court graph
+def x_shots(x, y, distance):
+    if (y > 47) & (distance < 48.5):
+        x = 25 + (25 - x)
+    elif (y < 47) & (distance > 48.5):
+        x = 25 + (25 - x)
+    else:
+        x = x
+    return x
+
+
+def y_shots(y, distance):
+    if (y > 47) & (distance < 48.5):
+        y = 47 + (47 - y)
+    elif (y < 47) & (distance > 48.5):
+        y = 47 + (47 - y)
+    else:
+        y = y
+    return y
+
+
+# We define shooting areas to classify shoots
+def area_defined(distance, x, y, type, longShots=27):
+    if distance > longShots:
+        return '3pt_Long_Shots'
+    elif '3pt' in type:
+        if (x < 25) & (y <= 14):
+            return '3pt_Left_Corner'
+        elif (x > 25) & (y <= 14):
+            return '3pt_Right_Corner'
+        elif x <= 14.3:
+            return '3pt_Top_Left'
+        elif x >= 35.7:
+            return '3pt_Top_Right'
+        else:
+            return '3pt_Middle'
+    elif ~('free throw' in type):
+        if (y <= 14) & (x < 17):
+            return "2pt_Left_Corner"
+        elif (y <= 14) & (x > 33):
+            return "2pt_Right_Corner"
+        elif x < 17:
+            return "2pt_Top_Left"
+        elif x > 33:
+            return "2pt_Top_Right"
+        elif y <= 9.25:
+            return "Under_the_Circle"
+        elif y <= 19:
+            return "Short_Paint_Shot"
+        else:
+            return "Long_Paint_Shot"
